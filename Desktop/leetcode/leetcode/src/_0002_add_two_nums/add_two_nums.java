593dfd7889c24b7bd131ddd72ffbb027a32d1d7c
huangzy1218 <2691386627@qq.com>
2023-01-04 20:53:58 +0800

    normal commit'

--- Desktop/leetcode/leetcode/src/_0002_add_two_nums/add_two_nums.java
+++ Desktop/leetcode/leetcode/src/_0002_add_two_nums/add_two_nums.java
@@ -1,12 +1,13 @@
 /**
- * You are given two non-empty linked lists representing two non-negative integers.
- * The digits are stored in reverse order, and each of their nodes contains a single digit.
- * Add the two numbers and return the sum as a linked list.
- * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
- * Example:
- * Input: l1 = [2,4,3], l2 = [5,6,4]
- * Output: [7,0,8]
- * Explanation: 342 + 465 = 807.
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
+ * 来源：力扣（LeetCode）
  */
 
 package _0002_add_two_nums;
@@ -25,7 +26,7 @@ class Solution {
         ListNode head = null, tail = null;
         int carry = 0; // 进位
         while (l1 != null || l2 != null) {
-            int n1 = l1 != null ? l1.val : 0; // 若无数字补0
+            int n1 = l1 != null ? l1.val : 0; // 若无数字补 0
             int n2 = l2 != null ? l2.val : 0;
             int sum = n1 + n2 + carry; // 和为进位 + 两链表对应位的数之和
             if (head == null) { // 头结点保存 digit
